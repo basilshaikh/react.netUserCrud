b02129a4fbece3b9445d59c269e37a5b3b01a964
basilshaikh <basilshaikh16@gmail.com>
2024-09-19 14:37:25 +0500

    moved folders

--- react.netUserCrud/userApplication/userApplication/Data/DatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace userApplication.Data
-{
-    public class DatabaseSettings
-    {
-        public string CollectionName { get; set; }
-        public string DatabaseName { get; set; }
-        public string Connection { get; set; }
-    }
-}
---
+++ userApplication/userApplication/Data/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace userApplication.Data
+{
+    public class DatabaseSettings
+    {
+        public string CollectionName { get; set; }
+        public string DatabaseName { get; set; }
+        public string Connection { get; set; }
+    }
+}
